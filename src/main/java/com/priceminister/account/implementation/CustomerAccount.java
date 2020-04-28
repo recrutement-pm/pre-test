0f980861bf176b4f57aa4419d05c2e6574ba8408
Nesrine <inanous@Nesrines-MacBook-Pro.local>
2020-04-28 12:34:03 +0200

    Implement add method in CustomerAccount class

--- src/main/java/com/priceminister/account/implementation/CustomerAccount.java
+++ src/main/java/com/priceminister/account/implementation/CustomerAccount.java
@@ -1,18 +1,52 @@
 package com.priceminister.account.implementation;
 
-import com.priceminister.account.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 
 public class CustomerAccount implements Account {
+	private final String number;
+	private Double balance = 0.0;
+	private AccountSettings accountSettings;
+	private MutableList<Operation> operations = Lists.mutable.empty();
 
-    public void add(Double addedAmount) {
-        // TODO Auto-generated method stub
-    }
+	public CustomerAccount() {
+		this.number = UUID.randomUUID().toString();
+		operations = Lists.mutable.of(new Operation(LocalDateTime.now(), OperationType.CREATION));
+	}
 
-    public Double getBalance() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public CustomerAccount(Double balance, AccountSettings accountSettings) {
+		this.number = UUID.randomUUID().toString();
+		this.balance = balance;
+		this.accountSettings = accountSettings;
+		operations = Lists.mutable.of(new Operation(LocalDateTime.now(), OperationType.CREATION));
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public AccountSettings getAccountSettings() {
+		return accountSettings;
+	}
+
+	public MutableList<Operation> getOperations() {
+		return operations;
+	}
+
+	public Double getBalance() {
+		return this.balance;
+	}
+
+	public void add(final Double addedAmount) {
+		if (addedAmount < 0) {
+			throw new IllegalArgumentException("addedAmount");
+		}
+		this.balance = this.balance + addedAmount;
+		Operation deposit = new MoneyTransaction(LocalDateTime.now(), OperationType.DEPOSIT, addedAmount, this.balance,
+				"ATM");
+		operations.add(deposit);
+	}
 
     public Double withdrawAndReportBalance(Double withdrawnAmount, AccountRule rule) 
     		throws IllegalBalanceException {
