4d95ec055a64701fbd07a44f2a1314f412d17084
Hennadii <erklareb@outlook.com>
2020-06-08 03:00:53 +0200

    added logger

--- src/main/java/com/priceminister/account/implementation/CustomerAccount.java
+++ src/main/java/com/priceminister/account/implementation/CustomerAccount.java
@@ -3,11 +3,14 @@ package com.priceminister.account.implementation;
 import com.priceminister.account.Account;
 import com.priceminister.account.AccountRule;
 import com.priceminister.account.IllegalBalanceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.UUID;
 
-
 public class CustomerAccount implements Account {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerAccount.class);
 
     private UUID id;
     private Double currentBalance;
@@ -24,6 +27,7 @@ public class CustomerAccount implements Account {
     public void add(Double addedAmount) {
         if (addedAmount > 0) {
             currentBalance += addedAmount;
+            LOGGER.info(currentBalance + " has increased on " + addedAmount);
         }
     }
 
@@ -33,10 +37,16 @@ public class CustomerAccount implements Account {
 
     public Double withdrawAndReportBalance(Double withdrawnAmount, AccountRule rule)
             throws IllegalBalanceException {
-        if (getBalance() <= 0) throw new IllegalBalanceException(getBalance());
-
+        if (getBalance() <= 0) {
+            LOGGER.info("Withdraw is restrict. Get balance is negative or equals 0");
+            throw new IllegalBalanceException(getBalance());
+        }
         Double withdrawResult = getBalance() - withdrawnAmount;
-        if (!rule.withdrawPermitted(withdrawResult)) throw new IllegalBalanceException(withdrawResult);
+        if (!rule.withdrawPermitted(withdrawResult)) {
+            LOGGER.info("Withdraw is restrict. " + "Withdraw amount: " + withdrawnAmount +
+                    " more than " + "current account balance " + getBalance());
+            throw new IllegalBalanceException(withdrawResult);
+        }
 
         currentBalance -= withdrawnAmount;
         return getBalance();
